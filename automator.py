b3dd7a12b2f18acfdd3279ed1ae343abb45f7c4f
openerror <evebillreg@gmail.com>
2016-07-02 12:28:42 +0100

    Added a few lines to automator.py to compute the (CPU) time needed for a simulation

--- automator.py
+++ automator.py
@@ -1,19 +1,27 @@
-import os, sys, subprocess32
+import os, sys, subprocess32, time
 
 cmd_list = [["make", "clean"],
 			["make", "dats"],
 			["python", "CfieldDataPrep.py", "cfield.dat"],
-			"ffmpeg -r 10 -f image2 -s 1024x768 -i ./FieldData/pngmovie/%d.png  -vcodec libx264 -crf 25  -pix_fmt yuv420p ./FieldData/pngmovie/cfield.mp4".split(),
+			"ffmpeg -r 10 -f image2 -s 1024x768 -i ./FieldData/pngmovie/%d.png  -vcodec libx264 -crf 25  -pix_fmt yuv420p ./cfield.mp4".split(),
 			["python", "ParticleDataPrep.py", "particles.dat"],
-			"ffmpeg -r 10 -f image2 -s 1024x768 -i ./ParticleData/pngmovie/%d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p ./ParticleData/pngmovie/particle.mp4".split(),
-			["rm", "cfield.dat", "particles.dat"]
+			"ffmpeg -r 10 -f image2 -s 1024x768 -i ./ParticleData/pngmovie/%d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p ./particle.mp4".split(),
+			["rm", "cfield.dat", "particles.dat"],
+			["python", "Quiver.py"],
+			"ffmpeg -r 10 -f image2 -s 1024x768 -i ./QuiverData/%d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p ./quiver.mp4".split()
 			]
 
 #sys.argv[1] is the folder in which the executable is located
 if __name__ == "__main__":
 	target_dir = sys.argv[1]
 	os.chdir(target_dir)
-	#print(os.getcwd())
+	#print(os.getcwd()) 
 	#os.system("cat %s" %executable_name)
+	begin = time.clock()
+	
 	for command in cmd_list:
 		subprocess32.call(command)
+		
+	end = time.clock()
+	time_taken = end - begin
+	print("\nThe simulation ran for %f" %time_taken)
